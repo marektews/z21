c8db6b39eda711489e2a56e9c895929c54478cc9
Marek Tews <marek@trx.com.pl>
2015-11-26 15:54:58 +0100

    More description of the basic functions of the programming interface.

--- Z21LanProtocol/Z21LanProtocol.h
+++ Z21LanProtocol/Z21LanProtocol.h
@@ -16,14 +16,44 @@ public:
     Z21LanProtocol();
     ~Z21LanProtocol();
 
+    /**
+     * @brief Configure and initialize internal threads and sockets
+     * @param address Z21 host address, default is 192.168.0.111
+     * @param port Z21 host port number, default is 21105
+     * @return true if success
+     */
     bool Configure(const QHostAddress& address = QHostAddress("192.168.0.111"), quint16 port = 21105);
 
 // SYSTEM, STATUS, VERSION
 public:
-    bool GetSerialNumber();                 // LAN_GET_SERIAL_NUMBER request
+    /**
+     * @brief GetSerialNumber Read serial number
+     * Sending request to z21. This routine is non-blocking.
+     * The lan_SerialNumber signal is broadcasting after receiving the response from z21.
+     * @return true if success
+     */
+    bool SendGetSerialNumber();                 // LAN_GET_SERIAL_NUMBER request
+    /**
+     * @brief Logoff
+     * Sending request to z21. This routine is non-blocking.
+     * After that Z21 no longer sends any response.
+     * @return true if success
+     */
     bool Logoff();                          // LAN_LOGOFF request
-
+    /**
+     * @brief GetBroadcastFlags
+     * Sending request to z21. This routine is non-blocking.
+     * The lan_BroadcastFlags signal is broadcasting after receiving the response from z21.
+     * @return true if success
+     */
     bool GetBroadcastFlags();               // LAN_GET_BROADCASTFLAGS request
+    /**
+     * @brief SetBroadcastFlags
+     * Sending request to z21. This routine is non-blocking.
+     * @param flags
+     * @param mask
+     * @return true if success
+     */
     bool SetBroadcastFlags(quint32 flags, quint32 mask);  // LAN_SET_BROADCASTFLAGS request
 
 signals:
@@ -33,6 +63,10 @@ signals:
 
 // X-BUS
 public:
+    /**
+     * @brief XBus_GetVersion
+     * @return true if success
+     */
     bool XBus_GetVersion();          // LAN_X_GET_VERSION request
 
 signals:
