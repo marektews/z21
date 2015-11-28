1e94430d539324b2a0b7e240e1ff11049fee471e
Marek Tews <marek.tews@gmail.com>
2015-11-28 20:21:33 +0100

    Creating new Z21CRC class to using in many different places

---
+++ Z21LanProtocol/Z21Crc.cpp
@@ -0,0 +1,33 @@
+﻿#include "Z21Crc.h"
+
+/**
+ * @brief Z21Crc::CRC
+ * @param v
+ * @param start
+ */
+void Z21Crc::Calculate(quint8 v, bool start)
+{
+    if(start)
+        crc = v;
+    else
+        crc ^= v;
+}
+
+/**
+ * @brief Z21Crc::Value
+ * @return
+ */
+quint8 Z21Crc::Value() const
+{
+    return crc;
+}
+
+/**
+ * @brief Z21Crc::CheckCRC
+ * @param crc
+ * @return
+ */
+bool Z21Crc::Check(quint8 crc)
+{
+    return this->crc == crc;
+}
---
+++ Z21LanProtocol/Z21Crc.h
@@ -0,0 +1,18 @@
+﻿#ifndef Z21CRC_H
+#define Z21CRC_H
+
+#include <QtCore>
+
+class Z21Crc
+{
+public:
+    Z21Crc() = default;
+    void Calculate(quint8 v, bool start = false);
+    quint8 Value() const;
+    bool Check(quint8 crc);
+
+private:
+    quint8 crc = 0;
+};
+
+#endif // Z21CRC_H
